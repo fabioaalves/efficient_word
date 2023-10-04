920c4e2dcf27f1bef3a7d7a96088c82ab1be166b
Fabio Alves <fabioarbaiter@gmail.com>
2023-10-04 20:10:18 -0300

    Create efficient word

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from glob import glob
+
+setup(
+    name='efficient-word',
+    version='2.0',
+    description='Hotword detection',
+    url='https://github.com/fabioaalves/efficient-word',
+    packages=['efficient-word'],
+    install_requires=open("./requirements.txt", 'r').read().split("\n"),
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+    include_package_data=True,
+)
