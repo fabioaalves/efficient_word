0a814109143fb4d3d01b755279fbe68f6f14a838
Fabio Alves <fabioarbaiter@gmail.com>
2023-10-04 20:35:42 -0300

    Fix package name

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from glob import glob
 
 setup(
-    name='efficient-word',
+    name='efficient_word',
     version='2.0',
     description='Hotword detection',
     url='https://github.com/fabioaalves/efficient-word',
-    packages=['efficient-word'],
+    packages=['efficient_word'],
     install_requires=open("./requirements.txt", 'r').read().split("\n"),
     classifiers=[
         'Programming Language :: Python :: 3.9',
